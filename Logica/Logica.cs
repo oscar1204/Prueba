6647cb477d06e37417ebccf364c0ec74c49f698b
oscar1204 <38057674+oscar1204@users.noreply.github.com>
2021-07-07 20:35:29 -0500

    Add project files.

---
+++ Logica/Logica.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Datos;
+
+
+namespace Logica
+{
+   public class Logica
+    {
+        Datos.Datos Con = new Datos.Datos();
+        public string Autor(String a)
+        {
+            String variables;
+            variables = a;
+            return variables;
+        }
+        public string Libro(String l)
+        {
+            String variable;
+            variable = l;
+            return variable;
+        }
+    }
+}
---
+++ Prueba/Views/Libro.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Libro.aspx.cs" Inherits="Prueba.Views.Libro" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
